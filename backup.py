ececcce6964986cee10e7cf03632e0a1b633f701
ian_nek <tky.naito@gmail.com>
2020-11-17 16:08:05 +0900

    fix

--- backup.py
+++ backup.py
@@ -2,6 +2,7 @@
 import json
 import requests
 import datetime
+import re
 
 class SlackBackup():
     '''
@@ -39,42 +40,64 @@ class SlackBackup():
         '''
         Slackにリクエスト送ってメッセージ一覧取得(時間指定なし(上限が10000件なのでlimit=10000なら全部取れる))
         '''
+        channel_messsages = {}
+        print('start ==get messages by slack==')
         for channel in self.target_channel:
+            channel_name = self._get_slack_conversation_info(channel)
+            print('channel {}'.format(channel_name))
             members = self._get_slack_user_list(channel)
             user_list = self._make_member_dir(members)
-            body_list = {}
-            body = ''
-            channel_name = self._get_slack_conversation_info(channel)
             message_list = self._get_slack_conversation_history(channel)
-            print("message_list")
-            print(len(message_list))
-            count = 0
+            body_list = []
             for messages in message_list:
                 for message in messages:
-                    body += '\n' + '======'
+                    post_list = []
+                    body_messages = {}
+                    message_time = message['ts'].split('.')
+                    post_time = str(datetime.datetime.fromtimestamp(int(message_time[0])))
+                    body_messages['post_time'] = post_time
                     if 'reply_count' in message:
+                        body_messages['reply'] = True
                         reply_messages = self._get_slack_conversation_replys(channel, message['ts'])
                         for reply_message in reply_messages:
-                            body = self._make_slack_body(reply_message, user_list, body)
+                            reply_post = {}
+                            reply_post_time, reply_post_body = self._make_slack_body(reply_message, user_list)
+                            reply_post['post_time'] = reply_post_time
+                            reply_post['post_body'] = reply_post_body
+                            post_list.append(reply_post)
+                        body_messages['post'] = post_list
                     else:
-                        body = self._make_slack_body(message, user_list, body)
-                count += 1
-                print(count)
-            body_list[channel_name] = body
-        return body_list
+                        body_messages['reply'] = False
+                        _, post_body = self._make_slack_body(message, user_list)
+                        body_messages['post'] = post_body
+                    body_list.append(body_messages)
+                body_list_sorted_by_time = sorted(body_list, key=lambda x: x['post_time'])
+            channel_messsages[channel_name] = body_list_sorted_by_time
+        print('end ==get messages by slack==')
+        return channel_messsages
 
-    def _make_slack_body(self, message, user_list, body):
+    def _make_slack_body(self, message, user_list):
         time = message['ts'].split('.')
+        post_body = []
+        post_time = str(datetime.datetime.fromtimestamp(int(time[0])))
+        post_id = ""
         if 'user' in message:
             user_id = message['user']
-            post_info = str(datetime.datetime.fromtimestamp(int(time[0]))) + " " + user_list[user_id]
-        else:
-            post_info = str(datetime.datetime.fromtimestamp(int(time[0])))
-        post_body = str(message['text'])
-        if '```' in post_body:
-            post_body = post_body.replace('```', '\n```\n')
-        body += '\n' + post_info + '\n' + post_body
-        return body
+            post_id = user_list[user_id]
+        post_body.append(post_id)
+        post_message = str(message['text'])
+        if '```' in post_message:
+            post_message = post_message.replace('```', '\n```\n')
+        elif post_message == '':
+            post_message = 'None'
+        mention_list = re.findall(r'<@\w*>', post_message)
+        if mention_list:
+            for mention in mention_list:
+                user_mention_id = re.search(r'\w+', mention).group()
+                mention_user = user_list[user_mention_id]
+                post_message = post_message.replace(mention, '@' + mention_user)
+        post_body.append(post_message)
+        return post_time, post_body
 
     def _get_slack_user_list(self, channel):
         '''
@@ -148,24 +171,46 @@ class SlackBackup():
         '''
         Growiに記事作成
         '''
-        for channel in request_message.keys():
+        print('start ==post messages to growi==')
+        for channel_name in request_message.keys():
             try:
-                request_body = request_message[channel]
                 if self.backup_period != 0:
                     oldest = datetime.datetime.now() - datetime.timedelta(days=self.backup_period)
                     period_path = oldest.strftime("%Y-%m-%d") + 'to' + datetime.datetime.today().strftime("%Y-%m-%d")
                 else:
                     period_path = 'oldest' + 'to' + datetime.datetime.today().strftime("%Y-%m-%d")
-                path = self.growi_article_path + channel + '-' + period_path
+                request_body = self._post_body_growi(request_message[channel_name], period_path)
+                path = self.growi_article_path + channel_name + '/' + period_path
                 url = self.growi_uri + '_api/pages.create'
-                print(path)
                 r = requests.post(url, data={'body': request_body, 'path': path, 'access_token': self.growi_token})
                 print(r)
                 print(r.json())
             except Exception as e:
                 print('error')
                 print(e)
+        print('end ==post messages to growi==')
         return
+
+    def _post_body_growi(self, messages_list, title):
+        '''
+        Growi投稿用のメッセージ作成
+        '''
+        body = title + '\n'
+        for message in messages_list:
+            body += '======' + '\n'
+            if message['reply']:
+                for reply_message in message['post']:
+                    body += reply_message['post_time'] + " " + reply_message['post_body'][0]
+                    body += '\n'
+                    body += reply_message['post_body'][1]
+                    body += '\n'
+            else:
+                body += message['post_time']  + " " + message['post'][0]
+                body += '\n'
+                body += message['post'][1]
+                body += '\n'
+        body += '======'
+        return body
 
     def backup(self):
         '''
@@ -175,8 +220,8 @@ class SlackBackup():
         '''
         self._read_settings()
         request_messages = self._get_slack_channel()
-        #self._post_growi(request_messages)
+        self._post_growi(request_messages)
 
 if __name__ == '__main__':
-    aaa = SlackBackup()
-    aaa.backup()
+    slackbackup = SlackBackup()
+    slackbackup.backup()
